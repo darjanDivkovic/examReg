12797f4687aeaa3ee8117d83f343a79109e8f44c
Darjan Divkovic <daarjan.tz@gmail.com>
2020-06-06 15:45:11 +0200

    Added some simple client-side verification for login

---
+++ script/login-validation.js
@@ -0,0 +1,14 @@
+
+function tryLogIn(){
+    let username = document.querySelector('[name="username"]').value;
+    let password = document.querySelector('[name="password"]').value;
+    
+    // If username or password field id empty, give err msg and deny submit
+    if(username === '' || password === ''){
+        document.getElementById("loginErrorMsg").innerHTML = "Username & password field can't be empty!";
+        return false;
+    };
+
+    return true;
+}
+
